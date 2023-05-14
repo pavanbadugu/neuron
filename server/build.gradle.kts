b082bd984689315011c80b848ca4125f8a5e20c1
Pavan Kumar <pavan.badugucse@gmail.com>
2023-05-14 23:06:46 +0530

    removed micronaut created git

---
+++ server/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
+    id("org.jetbrains.kotlin.kapt") version "1.6.21"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.6.21"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("io.micronaut.application") version "3.7.9"
+    id("io.micronaut.test-resources") version "3.7.9"
+}
+
+version = "0.1"
+group = "in.coderpro.neuron"
+
+val kotlinVersion = project.properties.get("kotlinVersion")
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    kapt("io.micronaut:micronaut-http-validation")
+    kapt("io.micronaut.serde:micronaut-serde-processor")
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("io.micronaut:micronaut-jackson-databind")
+    implementation("io.micronaut.flyway:micronaut-flyway")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-extension-functions")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("io.micronaut.sql:micronaut-jdbc-hikari")
+    implementation("jakarta.annotation:jakarta.annotation-api")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+    runtimeOnly("mysql:mysql-connector-java")
+    runtimeOnly("org.flywaydb:flyway-mysql")
+    runtimeOnly("org.slf4j:slf4j-simple")
+}
+
+application {
+    mainClass.set("in.coderpro.neuron.ApplicationKt")
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("17")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+}
+graalvmNative.toolchainDetection.set(false)
+micronaut {
+    runtime("netty")
+    testRuntime("kotest")
+    processing {
+        incremental(true)
+        annotations("in.coderpro.neuron.*")
+    }
+    testResources {
+        additionalModules.add("jdbc-mysql")
+    }
+}
+
+
+
---
+++ server/src/test/kotlin/in/coderpro/neuron/ServerTest.kt
@@ -0,0 +1,13 @@
+package `in`.coderpro.neuron
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.kotest.annotation.MicronautTest
+import io.kotest.core.spec.style.StringSpec
+
+@MicronautTest
+class ServerTest(private val application: EmbeddedApplication<*>) : StringSpec({
+
+    "test the server is running" {
+        assert(application.isRunning)
+    }
+})
